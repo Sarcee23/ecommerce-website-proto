560f175aeea9b0f46a773aaf65b291b9836a5757
Sarcee23 <146372411+Sarcee23@users.noreply.github.com>
2024-06-27 02:03:36 +0530

    Update ListProduct.jsx
    changed url to host

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -3,61 +3,63 @@ import './ListProduct.css';
 import cross_icon from '../../assets/cross_icon.png';
 
 const ListProduct = () => {
-    const [allproducts, setAllProducts] = useState([]);
+  const [allproducts, setAllProducts] = useState([]);
 
-    const fetchInfo = async () => {
-        await fetch('http://localhost:4000/allproducts')
-            .then((res) => res.json())
-            .then((data) => {
-                setAllProducts(data);
-            });
-    };
+  const url = "https://ecommerce-website-proto-backend.onrender.com";
 
-    useEffect(() => {
-        fetchInfo();
-    }, []);
+  const fetchInfo = async () => {
+    await fetch(`${url}/allproducts`)
+      .then((res) => res.json())
+      .then((data) => {
+        setAllProducts(data);
+      });
+  };
 
-    const remove_product = async (id) => {
-        await fetch('http://localhost:4000/removeproduct', {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ id: id })
-        });
-        fetchInfo(); // Fetch the updated list of products after removing
-    }
+  useEffect(() => {
+    fetchInfo();
+  }, []);
 
-    return (
-        <div className='list-product'>
-            <h1>All Product List</h1>
-            <div className="listproduct-format-main">
-                <p>Products</p>
-                <p>Title</p>
-                <p>Old Price</p>
-                <p>New Price</p>
-                <p>Category</p>
-                <p>Remove</p>
+  const remove_product = async (id) => {
+    await fetch(`${url}/removeproduct`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id: id })
+    });
+    fetchInfo(); // Fetch the updated list of products after removing
+  }
+
+  return (
+    <div className='list-product'>
+      <h1>All Product List</h1>
+      <div className="listproduct-format-main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Old Price</p>
+        <p>New Price</p>
+        <p>Category</p>
+        <p>Remove</p>
+      </div>
+      <div className="listproduct-allproducts">
+        <hr />
+        {allproducts.map((product, index) => {
+          return (
+            <div key={index} className="listproduct-format-main listproduct-format">
+              <img src={product.image} className='listproduct-product-icon' alt={product.name} />
+              <p>{product.name}</p>
+              <p>Rs {product.old_price}</p>
+              <p>Rs {product.new_price}</p>
+              <p>{product.category}</p>
+              <img onClick={() => { remove_product(product.id) }} src={cross_icon} className='listproduct-remove-icon' alt="Remove" />
+              <hr />
             </div>
-            <div className="listproduct-allproducts">
-                <hr />
-                {allproducts.map((product, index) => {
-                    return (
-                        <div key={index} className="listproduct-format-main listproduct-format">
-                            <img src={product.image} className='listproduct-product-icon' alt={product.name} />
-                            <p>{product.name}</p>
-                            <p>Rs {product.old_price}</p>
-                            <p>Rs {product.new_price}</p>
-                            <p>{product.category}</p>
-                            <img onClick={() => { remove_product(product.id) }} src={cross_icon} className='listproduct-remove-icon' alt="Remove" />
-                        <hr/>
-                        </div>
-                    );
-                })}
-            </div>
-        </div>
-    );
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default ListProduct;
