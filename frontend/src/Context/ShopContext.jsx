15b571659e306e4734890091d35cfe3b532ba35d
Sarcee23 <146372411+Sarcee23@users.noreply.github.com>
2024-06-27 01:55:56 +0530

    Update ShopContext.jsx
    Changed backend url to host

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,72 +1,74 @@
-
 import React, { createContext, useState, useEffect } from "react";
-
 
 export const ShopContext = createContext(null);
 
 const getDefaultCart = () => {
   let cart = {};
-  for (let index = 0; index < 300+1; index++) {
+  for (let index = 0; index < 300 + 1; index++) {
     cart[index] = 0;
   }
   return cart;
 };
 
 const ShopContextProvider = (props) => {
-  const[all_product,setAll_Product] = useState([]);
+  const [all_product, setAll_Product] = useState([]);
   const [cartItems, setCartItems] = useState(getDefaultCart());
 
+  const url = "https://ecommerce-website-proto-backend.onrender.com";
 
   useEffect(() => {
-   fetch('http://localhost:4000/allproducts')
-   .then((response)=>response.json())
-   .then((data)=>setAll_Product(data))
-   if(localStorage.getItem('auth-token')){
-    fetch('http://localhost:4000/getcart',{
-      method:'POST',
-      headers:{
-        Accept:'application/form-data',
-        'auth-token':`${localStorage.getItem('auth-token')}`,
-        'Content-Type':'application/json',
-      },
-      body:"",
-      }).then((response)=>response.json())
-      .then((data)=>setCartItems(data));
-    
-   }
-  }, []);
+    fetch(`${url}/allproducts`)
+      .then((response) => response.json())
+      .then((data) => setAll_Product(data));
+
+    if (localStorage.getItem('auth-token')) {
+      fetch(`${url}/getcart`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/form-data',
+          'auth-token': `${localStorage.getItem('auth-token')}`,
+          'Content-Type': 'application/json',
+        },
+        body: "",
+      })
+        .then((response) => response.json())
+        .then((data) => setCartItems(data));
+    }
+  }, [url]);
 
   const addToCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
-    if(localStorage.getItem('auth-token')){
-      fetch('http://localhost:4000/addtocart',{
-          method:'POST',
-          headers:{
-            Accept:'application/form-data',
-            'auth-token':`${localStorage.getItem('auth-token')}`,
-            'Content-Type' : 'application/json',
-          },
-          body:JSON.stringify({"itemId":itemId}),
+
+    if (localStorage.getItem('auth-token')) {
+      fetch(`${url}/addtocart`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/form-data',
+          'auth-token': `${localStorage.getItem('auth-token')}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ "itemId": itemId }),
       })
-      .then((response)=>response.json())
-      .then((data)=>console.log(data))
+        .then((response) => response.json())
+        .then((data) => console.log(data));
     }
   };
 
   const removeFromCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
-    if(localStorage.getItem('auth-token')){
-      fetch('http://localhost:4000/removefromcart',{
-        method:'POST',
-        headers:{
-          Accept:'application/form-data',
-          'auth-token':`${localStorage.getItem('auth-token')}`,
-          'Content-Type' : 'application/json',
+
+    if (localStorage.getItem('auth-token')) {
+      fetch(`${url}/removefromcart`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/form-data',
+          'auth-token': `${localStorage.getItem('auth-token')}`,
+          'Content-Type': 'application/json',
         },
-        body:JSON.stringify({"itemId":itemId}),
-    })
-    .then((response)=>response.json())
-    .then((data)=>console.log(data))
+        body: JSON.stringify({ "itemId": itemId }),
+      })
+        .then((response) => response.json())
+        .then((data) => console.log(data));
     }
   };
 
